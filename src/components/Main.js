50d8d753e1420d66ca6dfdc41c0b6ff059b2aed3
Pieter Koornhof <pieter@entelect.co.za>
2016-03-10 08:13:28 +0200

    Feature: Show Errors on page

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,12 +3,14 @@ require('styles/app.css');
 
 import React from 'react';
 import Navigation from '../containers/navigation/Navigation';
+import ErrorBar from '../components/error/ErrorBarComponent';
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
         <Navigation />
+        <ErrorBar />
         {this.props.children}
       </div>
     );
---
+++ src/components/error/ErrorBarComponent.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux'
+
+require('styles/error/ErrorBar.less');
+
+class ErrorBarComponent extends React.Component {
+  render() {
+    const {hasError, message} = this.props;
+    if (!hasError) {
+      return null;
+    }
+    return (
+      <div className="errorbar-component">
+        {message}
+      </div>
+    );
+  }
+}
+
+ErrorBarComponent.displayName = 'ErrorErrorBarComponent';
+
+ErrorBarComponent.propTypes = {
+  message: PropTypes.string.isRequired,
+  hasError: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = (state) => {
+  return state.error;
+}
+
+const ErrorBar = connect(
+  mapStateToProps
+)(ErrorBarComponent)
+
+export default ErrorBar
